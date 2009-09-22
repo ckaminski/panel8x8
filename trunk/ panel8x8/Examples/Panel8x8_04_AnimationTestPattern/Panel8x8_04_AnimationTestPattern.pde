5f51dcf5447ca819c015e5cda1cd0d718579aa43
DatamanLV <DatamanLV@786e6ffa-a6f0-11de-9ca8-173d21a9ba08>
2009-09-22 01:24:26 +0000

    Initial Version

---
+++ trunk/ panel8x8/Examples/Panel8x8_04_AnimationTestPattern/Panel8x8_04_AnimationTestPattern.pde
@@ -0,0 +1,70 @@
+// Panel8x8_04_AnimationTestPattern
+// Example 04: Manipulating the displays using programming.
+//
+// I needed something to test that all panels and all pixels were working.
+// This example demonstrates a simple test pattern.
+// We write directly to the output buffer and then display it.
+//
+// Kits & Schematics avaialbe at ModernDevice.Com
+//
+// By Dataman aka Charley Jones, 8x8Panel@CRJones.Com
+// 2009-09-17 Initial Version
+
+// Overrides default panel support
+// Set this value to the number of physical panels to support
+#define PANELS 1
+
+// Save some memory as we won't be needing text display
+#define PANEL8X8_NO_CHDATA
+
+// Required include for the Panel8x8 library
+#include <Panel8x8.h>
+
+// Declare an object of type Panel8x8
+Panel8x8 Panel;
+
+// No need to declare a buffer, 
+// We're going to draw directly into the display buffer, Panel.iScroll
+
+// Global value to remember which part of the pattern we are on.
+// 0-7 = horizontal line
+// 8-15 = vertical line
+int iPattern=0;
+
+// Called once by ardruino to setup the sketch
+void setup() {
+ // Call Panel.Begin to initialize panel buffers
+ // Syntax: buffer address, max buffer size, current text size, 0=Ram/1=Flash
+ Panel.Begin(0,0,0,0);  // Initialize with no buffers 
+ Panel.SetScrolling(0); // turn off scrolling
+ Panel.PanelMode=3;     // direct animation mode
+ Panel.frameDelay=100;  // speed this up!
+ Panel.ClearOutput();
+};
+
+// Called repeatedly by arduino
+// Loops through pattern 1 line a time
+// 0-7 = Horizontal Line, 8-15 = Vertical Line
+void loop() {
+  int i,j;
+  if (iPattern < 8) { //Horizontal
+   for (i=0;i<PANELS;i++) {
+     for (j=0; j<8; j++) {
+      Panel.iScroll[i][j] = 0;
+      if (j==iPattern) {Panel.iScroll[i][j]=255;}
+     }
+     Panel.iScroll[i][iPattern] = 255;
+   }
+  }
+  else {
+    for (i=0;i<PANELS;i++) { // Vertical
+      for (j=0;j<8;j++) {
+        Panel.iScroll[i][j]=0;
+        bitSet(Panel.iScroll[i][j],iPattern-8);
+      }
+    }
+  }
+  ++iPattern;
+  if (iPattern>15) {iPattern=0;}
+  Panel.Loop();
+};
---
+++ trunk/ panel8x8/Panel8x8.h
@@ -0,0 +1,97 @@
+// Panel8x8 Library
+// Simplifies programming the 8x8 LED Panel avaialable from ModernDevice.Com
+// See Examples and ReadMe.TXT
+// Kits & Schematics available at ModernDevice.Com
+//
+// By Dataman aka Charley Jones, 8x8Panel@CRJones.Com
+// 2009-09-19 V1.3 - Initial Release
+// 2009-09-18 V1.2 - Bug Fixes
+// 2009-09-17 V1.1 - Initial Version
+
+#ifndef Panel8x8b_h
+#define Panel8x8b_h
+
+#include "avr/pgmspace.h"            // Header to access ProgMem
+#include "..\Streaming\Streaming.h"  // Header to allow streaming operations String << "TEST"; Simplifies output.
+
+// -------------------- DEFINES ---------------------------
+
+// Switches, uncomment to enable and disable functionality
+//#define PANEL8X8_DUMPPANELS          // Dumps panel data to serial line after each loop 
+//                                        Does not init serial, must Serial.Begin(57600) in setup
+//#define PANEL8X8_NO_CHDATA           // Saves 760 of progmem by omitting text from Rom space, 
+//                                        no text scrolling though.
+
+// Configuration
+#ifndef PANELS          // Allows user to redefine # of supported panels on the fly
+#define PANELS       4  // The Max Number of panels (each panel consumes 8 bytes ram buffer spaces)
+#endif              
+#define LATCHPIN     4  // Pin connected to SPI latch
+#define OUTPUTENABLE 5  // Pin connected to SPI output enable - high equals off
+#define CLOCKPIN     6  // Pin connected to SPI clock - low to write new new data - high to transfer the data to the output buffers
+#define DATAPIN      7  // Pin connected to SPI clock
+
+// Animation File Version
+#define VERSION8X8MAJOR   1  // Current library version MAJOR.MINOR
+#define VERSION8X8MINOR   3  // Current library version
+
+// Defines
+#define LOWMASK    255  // The LOWMASK for stripping off High Bits
+#define HIGHBIT    128  // In 8 bits, the highest bit, left.
+#define LOWBIT       1  // In 8 bits, the lowest bit, right.
+
+
+class Panel8x8 { 
+
+ //Private Variables
+ // Globals
+ int i, j, k, l;                    // Generic Loop counters
+ int maxStringSize;                 // Size of the output message
+ boolean bIsScrolling;              // Turns on and off scrolling
+ byte bbuffer[16];                  // General purpose buffer
+ uint16_t *ptr16;                   // General 2 byte pointer
+ uint8_t *ptr8;                     // General 1 byte pointer
+ uint8_t tbyte;                     // General temporary 1 byte
+ prog_uint8_t iRawByte;             // General Pogram Memmory type 1 Byte;
+
+public:
+ byte     iPanels;                  // Current number of panels
+ uint16_t iFrames;                  // Number of animation frames
+ int idxPanel;                      // Current Panel Number
+ int idxLine;                       // The current line # 0-7
+ int idxMsg;                        // Where are we within the character text.
+ int idxScroll;                     // Current high water mark in Scrolling Matrix
+ int idxNext;                       // Trigger point for next letter
+ byte PanelMode;                    // Which mode are the panels in, 1=Text, 2=Animation, 3=Live
+
+ //Public Buffer Stuff
+ uint8_t *ramBuffer;                // Adddress if Ram memmory
+ //prog_uint8_t *progBuffer;        // Address if Program (flash) memmory
+ int          iBufferSize;          // Capacity of buffer including 0 byte
+ int          iBufferLen;           // Length of buffer
+ boolean isBufferProgMem;           // 0=Ram Buffer, 1=FLASH buffer
+ int frameDelay;                    // Default delay between refreshes
+ uint8_t iScroll[PANELS+2][8];      // The matrix for scrolling
+ 
+ Panel8x8();
+ 
+ void    About();                   // Dumps Version info to Serial                           
+ void    AddChar();                 // Adds 1 text character to panel ouput
+ void    Begin(char *Buffer, int pBufferSize, int pBufferLen, int pIsBufferProgMem);  // Defines buffers
+ char   *Binary8(uint8_t ibyte);    // Formats input as 8 digit binary text
+ void    ClearOutput();             // Clears the output buffer
+ void    DumpPanels();              // Dumps the output buffer contents in panel form to serial
+ void    FillOutput();              // Fill output buffer with text
+ uint8_t GetByte(int ib);           // Gets given byte from the input buffer
+ int     getNextLetter(int l);      // Gets next letter in the text input buffer
+ void    LoadAnimation();           // Loads next animation frame
+ void    Loop();                    // The main processing pump
+ void    NewMessage();              // Called whenever the text message is changed, resets buffers
+ void    Scroll();                  // Called to display & optionally scroll 
+ void    SetScrolling(short i);     // Called to set scrolling 0=off, 1=on
+};
+
+
+
+
+#endif Panel8x8b_h
