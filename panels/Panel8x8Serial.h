858caf32125633701f1a447b234bea112ae398b4
DatamanLV <DatamanLV@786e6ffa-a6f0-11de-9ca8-173d21a9ba08>
2009-09-28 07:21:39 +0000

    Supporting 8x8Serial

--- panels/Panel8x8Serial.h
+++ panels/Panel8x8Serial.h
@@ -8,7 +8,7 @@
 #ifndef Panel8x8Serialb_h
 #define Panel8x8Serailb_h
 
-#include <Panel8x8.h>                // Header to access Panel8x8 Library
+#include <Panel8x8.h>                  // Header to access Panel8x8 Library
 //#include "avr/pgmspace.h"            // Header to access ProgMem
 //#include "..\Streaming\Streaming.h"  // Header to allow streaming operations String << "TEST"; Simplifies output.
 //#include "..\String\WString.h"       // Header to allow string operations
@@ -24,29 +24,37 @@
 // Configuration
 
 // Animation File Version
-#define PANEL8X8SERIAL   1            // Current library version
+#define PANEL8X8SERIALMAJOR   1            // Current library version
+#define PANEL8X8SERIALMINOR   1            // Current library version
+
+#define PANEL8X8SERIALFILE    2            // Serial File Version Number
 
 // Defines
 
-class Panel8x8Serial::public Panel8x8 { 
-
-    Panel8x8Serial
-	 :Panel8x8() {
-  	  Serial.Beign(57600);
+class Panel8x8Serial:public Panel8x8 { 
+public:
+    Panel8x8Serial(){
+  	  Serial.begin(57600);
+          Panel8x8::Panel8x8();
     }
 
-	//Begin(char *buffer, int bufsize, int buflen, int isProgMem):
+    //Begin(char *buffer, int bufsize, int buflen, int isProgMem):
     //  Panel8x8::Begin(buffer,bufsize,buflen,isProgMem){
-	//	PanelMode = 3;
-	//}
+    //	PanelMode = 3;
+    //}
 
-    void loop(){
+    void About();
+
+    void Loop(){
 		if (!CheckSerial()) {
-			Panel8x8::loop();
+			Panel8x8::Loop();
 		}
 	}
 
-	int CheckSerial();
+    int CheckSerial();
+
+    int WriteByte(int idx, byte ib);     // Writes given byte to the input buffer
+
 };
 
 #endif Panel8x8Serialb_h
